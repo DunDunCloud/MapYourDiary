c5a2524d8f1f10b683529cfed7f3430306a3a9ab
Chan Yeong Yu <uchan01001@gmail.com>
2020-11-09 03:59:15 +0900

    DRF-MongoDB

---
+++ map/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers 
+from map.models import MapPost
+ 
+ 
+class MapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MapPost
+        fields = ('id',
+                  'title',
+                  'description',
+                  'published')
